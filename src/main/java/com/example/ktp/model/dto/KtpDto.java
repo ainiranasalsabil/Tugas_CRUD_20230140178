3c867ae63dae866b5fe079372ebc99453b074d5f
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-03-11 21:48:40 +0700

    menambahkan class KtpDto sebagai data transfer object untuk menampilkan data KTP antara service dan controller

---
+++ src/main/java/com/example/ktp/model/dto/KtpDto.java
@@ -0,0 +1,16 @@
+package com.example.ktp.model.dto;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class KtpDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private Date tanggalLahir;
+    private String jenisKelamin;
+
+}
